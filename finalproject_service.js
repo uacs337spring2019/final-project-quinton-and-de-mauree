b730caccf19aa57691ca5f27e5ce1140b32f68a0
quintonarnaud <qarnaud@email.arizona.edu>
2019-04-24 18:38:06 -0700

    Add files via upload

--- finalproject_service.js
+++ finalproject_service.js
@@ -95,32 +95,27 @@ app.get('/', function (req, res) {
 		let actorName = name.split("_");
 		let firstName = actorName[0];
 		let lastName = actorName[1];
-		con.connect(function(err) {
-		if (err) throw err;
 		console.log("Connected!");
 		con.query("SELECT m.name, m.year FROM actors a JOIN roles r ON a.id = r.actor_id JOIN movies m ON m.id = r.movie_id WHERE a.first_name='"+firstName+"' AND a.last_name = '"+lastName+"' ORDER BY m.year DESC;", 
-		                function (err, result, fields) {
-							if (err) throw err;
-							//database info
-							json.sql = result;
-							res.send(json);
-						});
-		});
+            function (err, result, fields) {
+				if (err) throw err;
+				//database info
+				json.sql = result;
+				res.send(json);
+			});
 	}
 	//user wants all actors in a movie
 	else if (mode === "movie"){
 		let movieName = name.replace("_", " ");
-		con.connect(function(err) {
-		if (err) throw err;
 		console.log("Connected!");
 		con.query("SELECT DISTINCT a.first_name, a.last_name FROM actors a JOIN roles r ON a.id = r.actor_id JOIN movies m ON m.id = r.movie_id WHERE m.name = '"+movieName+"' ORDER BY a.last_name, a.first_name;", 
-		                function (err, result, fields) {
-							if (err) throw err;
-							//database info
-							json.sql = result;
-							res.send(json);
-						});
-		});
+            function (err, result, fields) {
+				if (err) throw err;
+				//database info
+				json.sql = result;
+				res.send(json);
+			});
 	}
 });
+
 app.listen(3000);
