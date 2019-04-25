febb3118df61d80c9cd485aa411e7db26d097215
dllogan2 <46580738+dllogan2@users.noreply.github.com>
2019-04-24 21:10:34 -0700

    comments needed only

---
+++ finalproject_2.js
@@ -0,0 +1,334 @@
+(function() {
+	"use strict";
+	//when window loads
+	let url = "";
+	window.onload = function() {
+		document.getElementById("search").onclick = movieOrActor;
+	};
+
+	function movieOrActor () {
+		document.getElementById("success").innerHTML = "";
+		document.getElementById("comment_box").innerHTML = "";
+		let radios = document.getElementsByName("select");
+		let value = "";
+		for (let i = 0; i < radios.length; i++) {
+			if (radios[i].checked){
+				value = radios[i].value;
+			}
+		}
+		if (value === "movie"){
+			movieSearch();
+		}
+		else if (value === "actor"){
+			actorSearch();
+		}
+	}
+	function loadMessages() {
+		fetch(url)
+		.then(checkStatus)
+		.then(function(responseText) {
+			let json = JSON.parse(responseText);
+			let messages = json.messages;
+			let commentBox = document.getElementById("comment_box");
+			commentBox.innerHTML = "";
+			for (let i = 0; i < messages.length; i++) {
+				//parses messages in message.txt
+				//writes to html	
+				let message = messages[i];
+
+				let comment1 = document.createElement("div");
+				let comment2 = document.createElement("div");
+				comment1.innerHTML = message.name + ":";
+				comment2.innerHTML = message.comment;
+				comment1.style.marginBottom = "10px";
+				comment1.style.marginLeft = "5px";
+				comment1.style.marginRight = "5px";
+				comment2.style.marginBottom = "20px";
+				comment2.style.marginLeft = "15px";
+				comment1.style.marginRight = "5px";
+				comment2.style.fontStyle = "italic";
+				comment2.style.fontSize = "14pt";
+
+				let comment = document.createElement("div");
+				comment.appendChild(comment1);
+				comment.appendChild(comment2);
+				comment.style.marginLeft = "50px";
+				comment.style.marginRight = "50px";
+				comment.style.borderRadius = "20px";
+				comment.style.padding = "10px";
+				if (i % 2 == 0){
+					comment.style.backgroundColor = "#2B2725";
+				}
+				commentBox.appendChild(comment);
+			}
+			});
+	}
+	function send () {
+		let userName = document.getElementById("commenter").value;
+		let userMessage = document.getElementById("comment").value;
+		let name = document.getElementById("name").value;
+		const chat = {userName: userName, 
+	                 userMessage: userMessage,
+	             	 name: name};
+		const fetchOptions = {
+			method : 'POST',
+			headers : {
+				'Accept': 'application/json',
+				'Content-Type' : 'application/json'
+			},
+			body : JSON.stringify(chat)
+		};
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				//adds success message to html page
+				document.getElementById("success").style.color = "#46CC71";
+				document.getElementById("success").innerHTML = responseText;
+				loadMessages();
+			});
+		document.getElementById("comment").value = "";
+		document.getElementById("commenter").value = "";
+
+	}
+	//if user wants all movies
+	function movieSearch() {
+		let movieName = document.getElementById("name").value;
+		if (movieName != "") {
+			let name = movieName.replace(" ", "_");
+			url = "http://localhost:3000/?name="+name+"&mode=movie";
+			//checks database
+			fetch(url)
+			.then(checkStatus)
+			.then(function(responseText) {
+				if (responseText == undefined){
+					let p = document.getElementById("errormsg");
+					p.style.fontFamily = "Verdana";
+					p.style.color = "#CE3A3A";
+					p.innerHTML = "<br/>Movie not recognized, check your spelling and try again!";
+					document.getElementById("leavecomment").style.visibility = "hidden";
+					if (document.getElementById("table").innerHTML != "")
+					{
+						document.getElementById("h1").innerHTML = "";
+						document.getElementById("p").innerHTML = "";
+						document.getElementById("table").innerHTML = "";
+					}
+				}
+				else {
+					let json = JSON.parse(responseText);
+					let sql = json.sql;
+					if (sql.length == 0)
+					{
+						let p = document.getElementById("errormsg");
+						p.style.fontFamily = "Verdana";
+						p.style.color = "#CE3A3A";
+						p.innerHTML = "<br/>Movie not recognized, please try again!";
+						document.getElementById("leavecomment").style.visibility = "hidden";
+						if (document.getElementById("table").innerHTML != "")
+						{
+							document.getElementById("h1").innerHTML = "";
+							document.getElementById("p").innerHTML = "";
+							document.getElementById("table").innerHTML = "";
+						}
+					}
+					else {
+						//includes all movies in a table in descending order of the year
+						//creates txtfile
+						send(url);
+						document.getElementById("leavecomment").style.visibility = "visible";
+						document.getElementById("errormsg").innerHTML = "";
+						let content = document.getElementById("content");
+						let h1 = document.getElementById("h1");
+						let p = document.getElementById("p");
+						let table = document.getElementById("table");
+						h1.innerHTML = "";
+						p.innerHTML = "";
+						table.innerHTML = "";
+						table.innerHTML = "";
+						h1.innerHTML = "Results for " + movieName;
+						p.innerHTML = "Actors in "+ movieName;
+						console.log(json);
+						let row1 = document.createElement("tr");
+						let column = document.createElement("td");
+						column.innerHTML = "#";
+						row1.appendChild(column);
+						let column2 = document.createElement("td");
+						column2.innerHTML = "First Name";
+						row1.appendChild(column2);
+						let column3 = document.createElement("td");
+						column3.innerHTML ="Last Name";
+						row1.appendChild(column3);
+						table.appendChild(row1);
+						for (let i = 0; i < sql.length; i++) {
+							let firstName = sql[i].first_name;
+							let lastName = sql[i].last_name;
+							let row = document.createElement("tr");
+							let column = document.createElement("td");
+							column.innerHTML = i+1;
+							row.appendChild(column);
+							let column2 = document.createElement("td");
+							column2.innerHTML = firstName;
+							row.appendChild(column2);
+							let column3 = document.createElement("td");
+							column3.innerHTML = lastName;
+							row.appendChild(column3);
+							table.appendChild(row);
+						}
+						content.appendChild(table);
+
+						// let json = JSON.parse(responseText);
+						let messages = json.messages;
+						let commentBox = document.getElementById("comment_box");
+						commentBox.innerHTML = "";
+						for (let i = 0; i < messages.length; i++) {
+							//parses messages in message.txt
+							//writes to html	
+							let message = messages[i];
+							let name = document.createElement("p");
+							let comment = document.createElement("p");
+							name.innerHTML = message.name + ":";
+							comment.innerHTML = message.comment;
+							let commentName = document.createElement("div");
+							let commentMessage = document.createElement("div");
+							commentName.appendChild(name);
+							commentMessage.appendChild(comment);
+							commentBox.appendChild(commentName);
+							commentBox.appendChild(commentMessage);
+						}
+						document.getElementById("insert_comment").onclick = send;
+					}
+				}
+			});
+		}
+		else {
+			alert("Input movie name into textbox.");
+		}
+	}
+
+	function actorSearch() {
+		let actorName = document.getElementById("name").value;
+		if (actorName != "") {
+			let name = actorName.replace(" ", "_");
+			url = "http://localhost:3000/?name="+name+"&mode=actor";
+			console.log(url);
+			//checks database
+			fetch(url)
+			.then(checkStatus)
+			.then(function(responseText) {
+				if (responseText == undefined){
+					let p = document.getElementById("errormsg");
+					p.style.fontFamily = "Verdana";
+					p.style.color = "#CE3A3A";
+					p.innerHTML = "<br/>Actor not recognized, check your spelling and try again!";
+					document.getElementById("leavecomment").style.visibility = "hidden";
+					if (document.getElementById("table").innerHTML != "")
+					{
+						document.getElementById("h1").innerHTML = "";
+						document.getElementById("p").innerHTML = "";
+						document.getElementById("table").innerHTML = "";
+					}
+				}
+				else {
+					let json = JSON.parse(responseText);
+					let sql = json.sql;
+					if (sql.length == 0)
+					{
+						let p = document.getElementById("errormsg");
+						p.style.fontFamily = "Verdana";
+						p.style.color = "#CE3A3A";
+						p.innerHTML = "<br/>Actor not recognized, please try again!";
+						document.getElementById("leavecomment").style.visibility = "hidden";
+						if (document.getElementById("table").innerHTML != "")
+						{
+							document.getElementById("h1").innerHTML = "";
+							document.getElementById("p").innerHTML = "";
+							document.getElementById("table").innerHTML = "";
+						}
+					}
+					else {
+						//creates txtfile
+						console.log("here");
+						send(url);
+						document.getElementById("leavecomment").style.visibility = "visible";
+						document.getElementById("errormsg").innerHTML = "";
+						let content = document.getElementById("content");
+						let h1 = document.getElementById("h1");
+						let p = document.getElementById("p");
+						let table = document.getElementById("table");
+						//let img = document.getElementById("img");
+						h1.innerHTML = "";
+						p.innerHTML = "";
+						table.innerHTML = "";
+						//img.src = "";
+						let row1 = document.createElement("tr");
+						let column = document.createElement("td");
+						column.innerHTML = "#";
+						row1.appendChild(column);
+						let column2 = document.createElement("td");
+						column2.innerHTML = "Movie";
+						row1.appendChild(column2);
+						let column3 = document.createElement("td");
+						column3.innerHTML ="Year";
+						row1.appendChild(column3);
+						table.appendChild(row1);
+						h1.innerHTML = "Results for " + actorName;
+						p.innerHTML = "Films with "+ actorName;
+						console.log(h1,p);
+						for (let i = 0; i < sql.length; i++) {
+							let movieName = sql[i].name;
+							let year = sql[i].year;
+							let row = document.createElement("tr");
+							let column = document.createElement("td");
+							column.innerHTML = i+1;
+							row.appendChild(column);
+							let column2 = document.createElement("td");
+							column2.innerHTML = movieName;
+							row.appendChild(column2);
+							let column3 = document.createElement("td");
+							column3.innerHTML = year;
+							row.appendChild(column3);
+							table.appendChild(row);
+						}
+						content.appendChild(table);
+
+						let messages = json.messages;
+						let commentBox = document.getElementById("comment_box");
+						commentBox.innerHTML = "";
+						document.getElementById("success").innerHTML = "";
+						for (let i = 0; i < messages.length; i++) {
+							//parses messages in message.txt
+							//writes to html	
+							let message = messages[i];
+							let name = document.createElement("p");
+							let comment = document.createElement("p");
+							name.innerHTML = message.name + ":";
+							comment.innerHTML = message.comment;
+							let commentName = document.createElement("div");
+							let commentMessage = document.createElement("div");
+							commentName.appendChild(name);
+							commentMessage.appendChild(comment);
+							commentBox.appendChild(commentName);
+							commentBox.appendChild(commentMessage);
+						}
+						document.getElementById("insert_comment").onclick = send;
+					}
+				}
+			});
+			// .catch(function(error) {
+			// 	alert(error);
+			// });
+		}
+		//if no name entered
+		else {
+			alert("Input first and last name into textbox.");
+		}
+	}
+	// rejects the promise with a message including the status
+	function checkStatus(response) {  
+		if (response.status >= 200 && response.status < 300) {  
+            return response.text();
+        } 
+        else {  
+            return; 
+        } 
+	}
+})();
---
+++ finalproject_service_2.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const app = express();
+
+var mysql = require('mysql');
+
+const fs = require("fs");
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+               "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+
+// connection object
+let con = mysql.createConnection({
+  host: "mysql.allisonobourn.com",
+  database: "csc337imdb",
+  user: "csc337homer",
+  password: "d0ughnut",
+});
+
+console.log("connected");
+
+app.post('/', jsonParser, function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	//gets info from html
+	let userName = req.body.userName;
+	let userMessage = req.body.userMessage;
+	let name = req.body.name;
+	let fileName = name.replace(" ", "_");
+	let write = false;
+	//only writes to file if something written in both textboxes
+	if (userName != "" && userMessage != ""){
+		write = true;
+	}
+	//writes name and comment with ":::" in between, followed by new line
+	if (write) {
+		console.log("file created");
+		fs.appendFile(fileName+".txt", userName+":::"+userMessage+"\n", function(err) {
+			//send error message
+	    	if(err) {
+				console.log(err);
+				res.status(400);
+	    	}
+	    	//send success message to terminal
+	    	console.log("The file was saved!");
+	    	res.send("Success!");
+		});
+	}
+	else {
+		fs.appendFile(fileName+".txt", "", function(err) {
+			//send error message
+	    	if(err) {
+				console.log(err);
+				res.status(400);
+	    	}
+	    	//send nothing if commentor name or comment missing
+	    	res.send("");
+	    });
+	}
+});
+
+//give user info from database & gives information to user from text file
+app.get('/', function (req, res) { 
+	res.header("Access-Control-Allow-Origin", "*");
+	let params = req.query;
+	let mode = params.mode;
+	let name = params.name;
+	//reads file
+	let file = fs.readFileSync(name+".txt", 'utf8');
+	let lines = file.split("\n");
+	let json = {};
+	let messages = [];
+	//writes in json
+	for (let i = 0; i < lines.length-1; i++) {
+		let single = lines[i].split(":::");
+		let message = {};
+		message.name = single[0];
+		message.comment = single[1];
+		messages.push(message);
+	}
+	//comments from user
+	json.messages = messages;
+	
+	//user wants all movies an actor has been in
+	if(mode === "actor"){
+		let actorName = name.split("_");
+		let firstName = actorName[0];
+		let lastName = actorName[1];
+		console.log("Connected!");
+		con.query("SELECT m.name, m.year FROM actors a JOIN roles r ON a.id = r.actor_id JOIN movies m ON m.id = r.movie_id WHERE a.first_name='"+firstName+"' AND a.last_name = '"+lastName+"' ORDER BY m.year DESC;", 
+            function (err, result, fields) {
+				if (err) throw err;
+				//database info
+				json.sql = result;
+				res.send(json);
+			});
+	}
+	//user wants all actors in a movie
+	else if (mode === "movie"){
+		let movieName = name.replace("_", " ");
+		console.log("Connected!");
+		con.query("SELECT DISTINCT a.first_name, a.last_name FROM actors a JOIN roles r ON a.id = r.actor_id JOIN movies m ON m.id = r.movie_id WHERE m.name = '"+movieName+"' ORDER BY a.last_name, a.first_name;", 
+            function (err, result, fields) {
+				if (err) throw err;
+				//database info
+				json.sql = result;
+				res.send(json);
+			});
+	}
+});
+
+app.listen(3000);
